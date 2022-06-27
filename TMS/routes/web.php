be017cea7ba12e2542f0bc8ba0e4276e0d10d4fd
Syed Salman Reza <71028588+syed-reza98@users.noreply.github.com>
2022-06-28 04:19:22 +0600

    Added Home Page

--- TMS/routes/web.php
+++ TMS/routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/home', function () {
+    return view('index');
+});
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
